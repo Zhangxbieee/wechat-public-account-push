cb76d0be94f0932b5dcfa5620468975c17eda476
Zhangxbieee <154957141+Zhangxbieee@users.noreply.github.com>
2024-04-01 23:15:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '984efb7524d9420179921c4e9e79c388',
 
-  PROVINCE: '浙江省',
-  CITY: '宁波',
+  PROVINCE: '宁波',
+  CITY: '鄞州区',
 
   USERS: [
     {
